18a1287aa28116013cec01dd3eb36d08c61a4983
Rezoan Shakil Prince <prince.cuet.77@gmail.com>
2023-02-27 20:24:46 +0600

    Rename all the files and reallocated all the files

--- 1.variable.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Data type: number, string, boolean, null, undefined, void, object, array, tuples, any.
-// integer & float is included in number.
-// Note: Don't use 'any' datatype.
-exports.__esModule = true;
-var a = 'ABC';
-var b = 22.3;
-var c = false;
