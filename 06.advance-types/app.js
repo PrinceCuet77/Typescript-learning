6c16a622abce41d9a43db7d7d56083399bd5a3f0
Rezoan Shakil Prince <rezoan.shakil@bjitgroup.com>
2023-03-06 08:55:52 +0600

    working on type guard

--- 06.advance-types/app.js
+++ 06.advance-types/app.js
@@ -1,6 +1,62 @@
-// The common is 'number' otherwise showing an error.
-var universal = 22;
-console.log(universal);
-// Note: 
-// If 'type' is 'object' type then intersecting two 'type' is like combination.
-// If 'type' is union type then intersecting two 'type' is like finding the common of that.
+// Type Guard: It's an idea
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        // This is called type guard
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log('Name: ', emp.name);
+    if ('privileges' in emp) {
+        // Javascript syntax that allows me to check if 'privileges' is a property of 'emp' then return true.
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
+    }
+}
+var e1 = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date()
+};
+printEmployeeInformation(e1);
+// Output:
+// Name:  Max
+// Privileges: create-server
+// Start Date: Mon Mar 06 2023 08:34:48 GMT+0600 (Bangladesh Standard Time)
+printEmployeeInformation({ name: 'Manu', startDate: new Date() });
+// Output:
+// Name:  Manu
+// Start Date: Mon Mar 06 2023 08:35:42 GMT+0600 (Bangladesh Standard Time)
+// ---------------------------------------- Class --------------------------------------------
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log('Driving...');
+    };
+    return Car;
+}());
+var Truck = /** @class */ (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log('Driving a truck...');
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log('Loading cargo... ' + amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if ('loadCargo' in vehicle) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- 06.advance-types/app.ts
+++ 06.advance-types/app.ts
@@ -1,12 +1,94 @@
-type Combination = string | number // Union type
-type Numeric = number | boolean
+// Type Guard: It's an idea or approach to checking if a certain property or method is existed before I try to use it.
 
-type Universal = Combination & Numeric // Intersecting (finding common)
+// ---------------------------------------- Custom type --------------------------------------------
+type Combinable = string | number
 
-// The common is 'number' otherwise showing an error.
-const universal: Universal = 22
-console.log(universal) // Output: 22
+function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        // This is called type guard
+        return a.toString() + b.toString()
+    }
+    return a + b
+}
 
-// Note:
-// If 'type' is 'object' type then intersecting two 'type' is like combination.
-// If 'type' is union type then intersecting two 'type' is like finding the common of that.
+type Admin = {
+    name: string
+    privileges: string[]
+}
+
+type Employee = {
+    name: string
+    startDate: Date
+}
+
+// ---------------------------------------- Object type --------------------------------------------
+type UnknownEmployee = Admin | Employee
+type ElevatedEmployee = Admin & Employee
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log('Name: ', emp.name)
+    if ('privileges' in emp) {
+        // Type guard
+        // Javascript syntax that allows me to check if 'privileges' is a property of 'emp' then return true.
+        console.log('Privileges: ' + emp.privileges)
+    }
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate)
+    }
+}
+
+const e1: ElevatedEmployee = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date(),
+}
+
+printEmployeeInformation(e1)
+// Output:
+// Name:  Max
+// Privileges: create-server
+// Start Date: Mon Mar 06 2023 08:34:48 GMT+0600 (Bangladesh Standard Time)
+
+printEmployeeInformation({ name: 'Manu', startDate: new Date() })
+// Output:
+// Name:  Manu
+// Start Date: Mon Mar 06 2023 08:35:42 GMT+0600 (Bangladesh Standard Time)
+
+// ---------------------------------------- Class --------------------------------------------
+class Car {
+    drive() {
+        console.log('Driving...')
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving a truck...')
+    }
+
+    loadCargo(amount: number) {
+        console.log('Loading cargo... ' + amount)
+    }
+}
+
+type Vehicle = Car | Truck
+
+const v1 = new Car()
+const v2 = new Truck()
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive()
+    // if ('loadCargo' in vehicle) { // Way: 01
+    if (vehicle instanceof Truck) {
+        // Type guard
+        // Way: 02
+        vehicle.loadCargo(1000)
+    }
+}
+
+useVehicle(v1)
+useVehicle(v2)
+// Output:
+// Driving...
+// Driving a truck...
+// Loading cargo... 1000
